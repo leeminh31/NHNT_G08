92b199acf2086b0bb5baf8f142b25d92afb0b5ce
yenbui6262 <yenbui6262@gmail.com>
2023-07-16 23:19:45 +0700

    view quản lý bài đăng và tài khoản

---
+++ NHNT_G08/NHNT_G08/Controllers/QuanLyBaiDangController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using NHNT_G08.Models;
+
+namespace NHNT_G08.Controllers
+{
+    public class QuanLyBaiDangController : Controller
+    {
+        
+        private readonly NHNTContext _context;
+
+        public QuanLyBaiDangController(NHNTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: bài đăng
+        public async Task<IActionResult> Index()
+        {
+            var listBD = await _context.tblPhong.ToListAsync();
+            foreach (var phong in listBD)
+            {
+                var taiKhoan = _context.tblTaiKhoan.First(p => p.maTaiKhoan == phong.maTaiKhoan);
+                phong.tenNguoiDang = taiKhoan.hoTenNguoiDung;
+            }
+
+            return View(listBD);
+        }
+    }
+}
---
+++ NHNT_G08/NHNT_G08/Controllers/QuanLyTaiKhoanController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using NHNT_G08.Models;
+
+namespace NHNT_G08.Controllers
+{
+    public class QuanLyTaiKhoanController : Controller
+    {
+        private readonly NHNTContext _context;
+
+        public QuanLyTaiKhoanController(NHNTContext context)
+        {
+            _context = context;
+        }
+
+        // GET: bài đăng
+        public async Task<IActionResult> Index()
+        {
+            var listTK = await _context.tblTaiKhoan.ToListAsync();
+            foreach (var taikhoan in listTK)
+            {
+                var dmtaiKhoan = _context.tblDmTaiKhoan.First(p => p.maDmTaiKhoan == taikhoan.maDmTaiKhoan);
+                taikhoan.tenLoaiTK = dmtaiKhoan.tenLoaiTK;
+            }
+
+            return View(listTK);
+        }
+    }
+}
